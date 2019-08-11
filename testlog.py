4bbdb34804ac7f4201406044db652f10f522289b
simonliu009 <toopoo@gmail.com>
2019-08-11 18:42:39 +0800

    更新用法

--- testlog.py
+++ testlog.py
@@ -1,15 +1,10 @@
-# python_log
+#-*- coding: utf-8 -*-
 
-用法：
-```python
 import log
 
-log = log.Log(filename='111.log', mode='a', limit= 20480, backup_count = 10, colorful = True)
+log = log.Log(filename='111.log', mode='a', limit= 512, backup_count = 10, colorful = True)
 log.debug('This is a debug level message')
 log.info('This is a info level message')
 log.warning('This is a warning level message')
 log.error('This is a error level message')
 log.critical('This is a critical level message')
-```
-
-
