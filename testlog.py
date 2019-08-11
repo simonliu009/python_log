d2e3b55a8dea4d5d91c2f8ade3b76458890d02df
simonliu009 <toopoo@gmail.com>
2019-08-11 22:20:02 +0800

    增加格式化输出的范例~

--- testlog.py
+++ testlog.py
@@ -1,10 +1,19 @@
 #-*- coding: utf-8 -*-
 
 import log
+import sys,os,datetime
 
-log = log.Log(filename='111.log', mode='a', cmdlevel = 'DEBUG', filelevel = 'INFO', limit= 512, backup_count = 10, colorful = True)
+log = log.Log(filename='111.log', mode='a', cmdlevel = 'DEBUG', filelevel = 'INFO', limit= 20480, backup_count = 10, colorful = True)
 log.debug('This is a debug level message')
 log.info('This is a info level message')
 log.warning('This is a warning level message')
 log.error('This is a error level message')
 log.critical('This is a critical level message')
+
+
+now = datetime.datetime.utcnow()
+oneMinuteAgo = (datetime.datetime.utcnow() - datetime.timedelta(minutes = 1))
+timestr1 = now.strftime("%Y%m%d%H%M")
+timestr2 = oneMinuteAgo.strftime("%Y%m%d%H%M")
+log.info('Now:%s',timestr1)
+log.info('One Minute Ago:%s',timestr2)
